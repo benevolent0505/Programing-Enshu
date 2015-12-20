3193f334907728eaa4d20214aebcc351b3db7293
KENN <ken@nac.local>
2015-12-20 16:27:36 +0900

    手札のカードを配置。これも自分と相手の場で鏡合わせにする予定。

--- src/main/java/views/HandsPanel.java
+++ src/main/java/views/HandsPanel.java
@@ -2,26 +2,64 @@ package views; /**
  * Created by ken on 2015/12/10.
  */
 
+import java.awt.*;
 import java.util.ArrayList;
 import javax.swing.*;
-import java.awt.image.BufferedImage;
+
 
 public class HandsPanel extends JPanel{
 
     //現実的な枚数
-    private static final int MAX_HANDS = 15;
+    private static final int MAX_HANDS = 7;
 
     //カードの画像データ
-    private ArrayList<BufferedImage> hand_images = new ArrayList<BufferedImage>();
-
+    private ArrayList<JButton> handsButton;
+    private JLabel handsLabel;
 
     public HandsPanel(String side){
         if(side.equals("self")){
-            JLabel self_hands = new JLabel("Self Hands");
-            this.add(self_hands);
+             handsLabel = new JLabel("Self Hands");
         } else if(side.equals("enemy")){
-            JLabel enemy_hands = new JLabel("Enemy Hands");
-            this.add(enemy_hands);
+             handsLabel = new JLabel("Enemy Hands");
         }
+
+        handsButton = new ArrayList<JButton>();
+
+        for(int i = 0; i < MAX_HANDS; i++){
+            JButton tmp = new JButton("Hand"+i);
+            handsButton.add(tmp);
+        }
+
+        GridBagLayout mainLayout = new GridBagLayout();
+        setLayout(mainLayout);
+        GridBagConstraints mainLayoutConstraints = new GridBagConstraints();
+        mainLayoutConstraints.fill = GridBagConstraints.BOTH;
+
+        //handsLabel
+        mainLayoutConstraints.weightx = 1.0;  //横の辺の比
+        mainLayoutConstraints.weighty = 0.3;
+        mainLayoutConstraints.gridx = 0;
+        mainLayoutConstraints.gridy = 0;
+        mainLayoutConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        mainLayoutConstraints.gridheight = 1;
+        mainLayoutConstraints.anchor = GridBagConstraints.CENTER;
+        mainLayout.setConstraints(handsLabel, mainLayoutConstraints);
+        add(handsLabel);
+
+        //handsButton
+        for(int i = 0; i<MAX_HANDS; i++) {
+            mainLayoutConstraints.weightx = 0.144;
+            mainLayoutConstraints.weighty = 0.7;
+            mainLayoutConstraints.gridx = i;
+            mainLayoutConstraints.gridy = 1;
+            mainLayoutConstraints.gridwidth = 1;
+            mainLayoutConstraints.gridheight = 1;
+            mainLayoutConstraints.anchor = GridBagConstraints.CENTER;
+            mainLayout.setConstraints(handsButton.get(i), mainLayoutConstraints);
+            add(handsButton.get(i));
+        }
+
+
+
     }
 }
