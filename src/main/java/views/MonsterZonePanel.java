e2d90edec2574532e6628c80e5e6054df1e9fa50
benevolent0505 <mikio.fjt@gmail.com>
2016-02-03 22:57:21 +0900

    Monster表示のメソッドを記述

--- src/main/java/views/MonsterZonePanel.java
+++ src/main/java/views/MonsterZonePanel.java
@@ -1,14 +1,53 @@
 package views;
 
+import listeners.CardButtonClickListener;
+import models.Card;
+import models.GameKeeper;
+import models.Player;
+import rx.functions.Action1;
+import rx.functions.Func1;
+import views.components.CardButton;
+
 import javax.swing.*;
-import java.awt.*;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  * Created by benevolent0505 on 1/21/16.
  */
-public class MonsterZonePanel extends JPanel {
+public class MonsterZonePanel extends JPanel implements Observer {
 
-    public MonsterZonePanel() {
+    private Player player;
 
+    public MonsterZonePanel(GameKeeper keeper) {
+        this.player = keeper.getPlayer();
+        player.setMonsterZoneObserver(this);
+
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+        setMonsters();
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        setMonsters();
+    }
+
+    private void setMonsters() {
+        rx.Observable.from(player.getMonsterZone())
+                .map(new Func1<Card, CardButton>() {
+                    @Override
+                    public CardButton call(Card card) {
+                        return new CardButton(card);
+                    }
+                })
+                .subscribe(new Action1<CardButton>() {
+                    @Override
+                    public void call(CardButton cardButton) {
+                        cardButton.addActionListener(new CardButtonClickListener(player, cardButton));
+                        add(cardButton);
+                        add(Box.createGlue());  // 間隔調節用
+                    }
+                });
     }
 }
